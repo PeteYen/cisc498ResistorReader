20bda847d2366c30889c72c8164e87c56efb8df5
ashley-liao <71025088+ashley-liao@users.noreply.github.com>
2023-03-25 14:10:29 -0400

    send read file to backend

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,92 +1,100 @@
-import "./Home.scss";
-import { useRef } from "react";
+import React, { useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import "./Home.scss";
 
 const Home = () => {
-  const inputRef = useRef(null);
+  const navigate = useNavigate();
 
-  const handleClick = () => {
-    // 👇️ open file input box on click of other element
-    inputRef.current.click();
-  };
+  const customRequest = async ({ file, onSuccess, onError }) => {
+      const reader = new FileReader();
+      reader.onload = async () => {
+        const dataURL = reader.result;
 
-  let navigate = useNavigate();
-  const handleFileChange = (event) => {
-    const fileObj = event.target.files && event.target.files[0];
-    if (!fileObj) {
-      return;
-    }
+        // Send the dataURL to the backend
+        try {
+          const response = await fetch("http://localhost:3001/upload", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ dataURL }),
+          });
 
-    const reader = new FileReader();
-    reader.onload = () => {
-      const dataURL = reader.result;
-      navigate(`/processing?img=${encodeURIComponent(dataURL)}`);
+          if (response.ok) {
+            navigate(`/processing?img=${encodeURIComponent(dataURL)}`);
+            onSuccess();
+          } else {
+            onError(new Error("Failed to upload image"));
+          }
+        } catch (error) {
+          onError(error);
+        }
+      };
+      reader.onerror = (error) => {
+        onError(error);
+      };
+      reader.readAsDataURL(file);
     };
-    reader.readAsDataURL(fileObj);
-  };
-
-  const routeChange = () => {
-    const constraints = {
-      audio: false,
-      video: true,
+    const sendPhotoToBackend = async (dataURL) => {
+      try {
+        const response = await fetch("http://localhost:3001/upload", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ dataURL }),
+        });
+  
+        if (response.ok) {
+          navigate(`/processing?img=${encodeURIComponent(dataURL)}`);
+        } else {
+          throw new Error("Failed to upload image");
+        }
+      } catch (error) {
+        console.error(error);
+      }
     };
-
-    navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then((stream) => {
-        const video = document.createElement("video");
-        video.srcObject = stream;
-        video.onloadedmetadata = () => {
-          video.play();
-          const canvas = document.createElement("canvas");
-          canvas.width = video.videoWidth;
-          canvas.height = video.videoHeight;
-          const ctx = canvas.getContext("2d");
-          ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-          const dataURL = canvas.toDataURL();
-          navigate(`/processing?img=${dataURL}`);
-        };
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
+  
+    const routeChange = () => {
+      const constraints = {
+        audio: false,
+        video: true,
+      };
+  
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then((stream) => {
+          const video = document.createElement("video");
+          video.srcObject = stream;
+          video.onloadedmetadata = () => {
+            video.play();
+            const canvas = document.createElement("canvas");
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            const ctx = canvas.getContext("2d");
+            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+            const dataURL = canvas.toDataURL();
+  
+            // Send the taken photo to the backend
+            sendPhotoToBackend(dataURL);
+          };
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    };
 
   return (
     <div>
       <div className="homeContainer">
-        {/*<div className="headerContainer">*/}
-        {/*    <div className="homeHeader">*/}
-        {/*        Home*/}
-        {/*    </div>*/}
-        {/*    <div className="Buttons">*/}
-        {/*        <button className="login">*/}
-        {/*            login*/}
-        {/*        </button>*/}
-        {/*        <button className="register">*/}
-        {/*            register*/}
-        {/*        </button>*/}
-        {/*    </div>*/}
-        {/*</div>*/}
-
         <div className="homeContent">
           <h2>Upload your image below</h2>
           <p>Read value from your resistor color stripes.</p>
           <div className="fileContainer">
-            <input
-              style={{ display: "none" }}
-              ref={inputRef}
-              onChange={handleFileChange}
-              type="file"
-            />
-            <button onClick={handleClick} className="imgButton btn">
-              Upload an image
-            </button>
-
+            <input type="file" onChange={(event) => customRequest(event)} />
             <button onClick={routeChange} className="imgButton btn">
               Take a picture by Phone
             </button>
-
             <button
               onClick={() => navigate("/camera")}
               className="imgButton btn"
@@ -95,7 +103,6 @@ const Home = () => {
             </button>
           </div>
         </div>
-
         <div className="homeFooter"></div>
       </div>
     </div>
