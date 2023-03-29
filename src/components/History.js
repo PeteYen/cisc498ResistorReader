5b83f7ee2cfa157b986f4d4b5e30db577f3e6490
oliverguo666 <59981589+oliverguo666@users.noreply.github.com>
2023-03-29 06:21:09 -0400

    add history page

--- src/components/History.js
+++ src/components/History.js
@@ -1,12 +1,23 @@
 import '@aws-amplify/ui-react/styles.css';
 import {Authenticator} from "@aws-amplify/ui-react";
+import "./History.scss"
 
 const History = () => {
+    const image1 = require('../res_img/img.png');
+    const image2 = require('../res_img/img_1.png');
+    const image3 = require('../res_img/img_2.png');
+    const images = [image1, image2, image3];
     return (
         <Authenticator>
             {({signOut}) => (
-                <div>
-                    <button onClick={signOut}>signOut</button>
+                <div className='historyContainer'>
+                    <button className='btn' onClick={signOut}>signOut</button>
+                    <div className="imageContainer">
+                        {images.map((image, index) => (
+                            <img src={image} alt={`Image ${index}`} key={`image${index}`} />
+                        ))}
+
+                    </div>
                 </div>
                 )}
         </Authenticator>
