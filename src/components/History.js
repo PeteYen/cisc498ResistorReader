3dc8e309ba0055302f8c48aadffae7d0b79cab9f
PeteYen <19jy28@queensu.ca>
2023-04-05 12:09:32 -0400

    trying

--- src/components/History.js
+++ src/components/History.js
@@ -1,26 +1,43 @@
-import '@aws-amplify/ui-react/styles.css';
-import {Authenticator} from "@aws-amplify/ui-react";
-import "./History.scss"
+import { useEffect, useState } from "react";
+import "@aws-amplify/ui-react/styles.css";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "./History.scss";
+import { Storage } from "@aws-amplify/storage";
 
 const History = () => {
-    const image1 = require('../res_img/img.png');
-    const image2 = require('../res_img/img_1.png');
-    const image3 = require('../res_img/img_2.png');
-    const images = [image1, image2, image3];
-    return (
-        <Authenticator>
-            {({signOut}) => (
-                <div className='historyContainer'>
-                    <button className='btn' onClick={signOut}>signOut</button>
-                    <div className="imageContainer">
-                        {images.map((image, index) => (
-                            <img src={image} alt={`Image ${index}`} key={`image${index}`} />
-                        ))}
+  const [imageUrls, setImageUrls] = useState([]);
 
-                    </div>
-                </div>
-                )}
-        </Authenticator>
-    );
+  useEffect(() => {
+    Storage.list("photos/")
+      .then((result) => {
+        const urls = result.map((item) => {
+          return Storage.get(item.key);
+        });
+        Promise.all(urls)
+          .then((data) => {
+            setImageUrls(data);
+          })
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <Authenticator>
+      {({ signOut }) => (
+        <div className="historyContainer">
+          <button className="btn" onClick={signOut}>
+            signOut
+          </button>
+          <div className="imageContainer">
+            {imageUrls.map((url, index) => (
+              <img src={url} alt={`Image ${index}`} key={`image${index}`} />
+            ))}
+          </div>
+        </div>
+      )}
+    </Authenticator>
+  );
 };
+
 export default History;
