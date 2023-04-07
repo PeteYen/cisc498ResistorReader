c5077ee9d8c157a4ebc845fd753fefc17c2af49e
PeteYen <19jy28@queensu.ca>
2023-04-07 11:00:19 -0400

    update

--- src/components/History.js
+++ src/components/History.js
@@ -1,25 +1,48 @@
+import React, { useState, useEffect } from 'react';
 import '@aws-amplify/ui-react/styles.css';
-import {Authenticator} from "@aws-amplify/ui-react";
-import "./History.scss"
-
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Storage } from 'aws-amplify';
+import "./History.scss";
 
 const History = () => {
-    const image1 = require('../res_img/img.png');
-    const image2 = require('../res_img/img_1.png');
-    const image3 = require('../res_img/img_2.png');
-    const images = [image1, image2, image3];
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        async function getImages() {
+            try {
+              const response = await Storage.list('');
+              if (!Array.isArray(response.results)) {
+                throw new Error('imageKeys is not an array');
+              }
+              const imageKeys = response.results;
+              const images = await Promise.all(
+                imageKeys.map(async (key) => {
+                  const image = await Storage.get(key.key);
+                  return image;
+                })
+              );
+              setImages(images)
+            } catch (error) {
+              console.error('Error while fetching images: ', error);
+              setImages([]);
+            }
+          }
+          
+        getImages();
+    }, []);
+
     return (
         <Authenticator>
             {({signOut}) => (
                 <div className='historyContainer'>
                     <button className='btn' onClick={signOut}>signOut</button>
                     <div className="imageContainer">
-                        {images.map((image, index) => (
-                            <img src={image} alt={`Image ${index}`} key={`image${index}`} />
+                        {images.map((imageUrl, index) => (
+                            <img src={imageUrl} alt={`Image ${index}`} key={`image${index}`} />
                         ))}
                     </div>
                 </div>
-                )}
+            )}
         </Authenticator>
     );
 };
