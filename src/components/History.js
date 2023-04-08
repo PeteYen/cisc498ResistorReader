b64e59182b169d0bd1d6807c4b2bad7c7926a8a2
PeteYen <19jy28@queensu.ca>
2023-04-07 22:10:48 -0400

    new function added

--- src/components/History.js
+++ src/components/History.js
@@ -6,11 +6,14 @@ import "./History.scss";
 
 const History = () => {
     const [images, setImages] = useState([]);
+    const [selectedImageKey, setSelectedImageKey] = useState(null);
+    const [isAdmin, setIsAdmin] = useState(false);
 
     useEffect(() => {
-        Auth.currentAuthenticatedUser().then(() => {
+        Auth.currentAuthenticatedUser().then(user => {
+            setIsAdmin(user.signInUserSession.accessToken.payload['cognito:groups']?.includes('Admin'));
             getImages();
-        })
+        });
     }, []);
 
     async function getImages() {
@@ -23,7 +26,7 @@ const History = () => {
             const images = await Promise.all(
                 imageKeys.map(async (key) => {
                     const image = await Storage.get(key.key);
-                    return image;
+                    return { key: key.key, image: image };
                 })
             );
             setImages(images)
@@ -32,15 +35,34 @@ const History = () => {
             setImages([]);
         }
     }
-    
+
+    async function deleteImage(key) {
+        try {
+            await Storage.remove(key);
+            setImages(images.filter(image => image.key !== key));
+            setSelectedImageKey(null);
+        } catch (error) {
+            console.error('Error while deleting image: ', error);
+        }
+    }
+
     return (
         <Authenticator>
             {({signOut}) => (
                 <div className='historyContainer'>
                     <button className='btn' onClick={signOut}>Sign Out</button>
                     <div className="imageContainer">
-                        {images.slice(1).map((imageUrl, index) => (
-                            <img src={imageUrl} alt={`Image ${index}`} key={`image${index}`} />
+                        {images.slice(1).map((image, index) => (
+                            <div key={image.key}>
+                                <img src={image.image} alt={`Image ${index}`} onClick={() => setSelectedImageKey(image.key)} />
+                                {isAdmin && selectedImageKey === image.key && (
+                                    <div className='deleteImageContainer'>
+                                        <p>Are you sure you want to delete this image?</p>
+                                        <button className='btn deleteImageConfirm' onClick={() => deleteImage(image.key)}>Yes</button>
+                                        <button className='btn deleteImageCancel' onClick={() => setSelectedImageKey(null)}>No</button>
+                                    </div>
+                                )}
+                            </div>
                         ))}
                     </div>
                 </div>
@@ -48,4 +70,5 @@ const History = () => {
         </Authenticator>
     );
 };
+
 export default History;
